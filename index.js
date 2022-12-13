f8df53f72467b690575f000faef415bbbb33c45e
markkhoo <mark@eplansoft.com>
2022-12-12 17:06:45 -0800

    ADD health check

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3003;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/", express.static("public"));
+
+// app.get("/", (req, res) => {
+//   return res.send("Hello World!");
+// });
+
+app.get("/health", (req, res) => {
+  return res.status(200).send(true);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
